4ede0ed4dbf23205a9e2ad30975ff7abb38b57f8
Mairbek Khadikov <mkhadikov@gmail.com>
2011-01-08 20:38:46 +0200

    Composite filter introduced.

---
+++ src/com/github/suiteconfig/junit/filter/CompositeFilter.java
@@ -0,0 +1,35 @@
+package com.github.suiteconfig.junit.filter;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.runner.Description;
+import org.junit.runner.manipulation.Filter;
+
+/**
+ * Groups JUnit filters.
+ *
+ * @author Mairbek Khadikov
+ */
+public class CompositeFilter extends Filter {
+    private final ImmutableSet<Filter> filters;
+
+    public CompositeFilter(Iterable<Filter> filters) {
+        this.filters = ImmutableSet.copyOf(filters);
+    }
+
+
+    @Override
+    public boolean shouldRun(Description description) {
+        for (Filter filter : filters) {
+            if (!filter.shouldRun(description)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String describe() {
+        return "CompositeFilter. Filters: " + filters;
+    }
+
+}
