5c2ac99c98c5e07660e5122f8288b48979e43f4d
Mairbek Khadikov <mkhadikov@gmail.com>
2011-01-08 20:38:57 +0200

    Always run filter introduced

---
+++ src/com/github/suiteconfig/junit/filter/Filters.java
@@ -0,0 +1,29 @@
+package com.github.suiteconfig.junit.filter;
+
+import org.junit.runner.Description;
+import org.junit.runner.manipulation.Filter;
+
+/**
+ * @author Mairbek Khadikov
+ */
+public class Filters {
+    private final static Filter ALWAYS_RUN = new Filter() {
+        @Override
+        public boolean shouldRun(Description description) {
+            return true;
+        }
+
+        @Override
+        public String describe() {
+            return "Always run filter";
+        }
+    };
+
+    private Filters() {
+    }
+
+    public static Filter alwaysRun() {
+        return ALWAYS_RUN;
+    }
+
+}
