90c3aaac9d6d97e00abe594778afdde52a260567
Mairbek Khadikov <mkhadikov@gmail.com>
2011-01-08 21:19:16 +0200

    Runner and runner builder implemented

---
+++ src/com/github/suiteconfig/junit/filter/ConfigurableRunnerBuilder.java
@@ -0,0 +1,54 @@
+package com.github.suiteconfig.junit.filter;
+
+import com.github.suiteconfig.junit.TestCaseRunner;
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+import org.junit.internal.builders.NullBuilder;
+import org.junit.rules.MethodRule;
+import org.junit.runner.Runner;
+import org.junit.runners.model.RunnerBuilder;
+import org.junit.runners.model.RunnerScheduler;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * {@link org.junit.runners.model.RunnerBuilder} implementation that allows to specify rules and runner scheduler.
+ *
+ * @author Mairbek Khadikov
+ */
+public class ConfigurableRunnerBuilder extends RunnerBuilder {
+    private final List<MethodRule> rules;
+    private final RunnerScheduler runnerScheduler;
+
+    public ConfigurableRunnerBuilder(List<MethodRule> rules, RunnerScheduler runnerScheduler) {
+        this.rules = ImmutableList.copyOf(rules);
+        this.runnerScheduler = runnerScheduler;
+    }
+
+    @Override
+    public Runner runnerForClass(Class<?> testClass) throws Throwable {
+        RunnerBuilder delegate = new NullBuilder();
+
+        if (isTestClass(testClass)) {
+            delegate = new RunnerBuilder() {
+                @Override
+                public Runner runnerForClass(Class<?> testClass) throws Throwable {
+                    return new TestCaseRunner(testClass, rules, runnerScheduler);
+                }
+            };
+        }
+        return delegate.runnerForClass(testClass);
+    }
+
+    private static boolean isTestClass(Class<?> testClass) {
+        Method[] methods = testClass.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Test.class)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/com/github/suiteconfig/junit/filter/FilterableSuite.java
@@ -0,0 +1,28 @@
+package com.github.suiteconfig.junit.filter;
+
+import org.junit.runner.manipulation.Filter;
+import org.junit.runner.manipulation.NoTestsRemainException;
+import org.junit.runners.Suite;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.RunnerBuilder;
+
+import java.util.Collection;
+
+/**
+ * A suite that supports filters.
+ *
+ * @author Mairbek Khadikov
+ */
+public class FilterableSuite extends Suite {
+
+    public FilterableSuite(RunnerBuilder builder, Collection<Class<?>> classes, Filter filter) throws InitializationError, NoTestsRemainException {
+        super(builder, classes.toArray(new Class<?>[classes.size()]));
+        filter(filter);
+    }
+
+    public FilterableSuite(RunnerBuilder builder, Collection<Class<?>> classes, Collection<Filter> filters) throws InitializationError, NoTestsRemainException {
+        super(builder, classes.toArray(new Class<?>[classes.size()]));
+        filter(new CompositeFilter(filters));
+    }
+
+}
